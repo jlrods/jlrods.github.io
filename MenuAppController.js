f3e47de2180c33ccf6d874ae0414503ed0f7c7b6
Jose Rodriguez <jlrods@gmail.com>
2016-11-14 21:37:55 +0000

    Add new files via upload

---
+++ MenuAppController.js
@@ -0,0 +1,26 @@
+﻿menuApp.controller('MenuController', ['$scope', function ($scope) {
+    $scope.subMenus = [
+        {
+            title: 'Home',
+            icon: 'home.png'
+        },
+        {
+            title: 'Education',
+            icon: 'education.png'
+        },
+        {
+            title: 'Career',
+            icon: 'career.png',
+        },
+        {
+            title: 'My Interests',
+            icon: 'interests.png'
+        },
+        {
+            title: 'Contact me',
+            icon: 'contact.png'
+        }
+    ];//End of array of data
+    $scope.text = 'Jose Leonardo'
+    }
+]);//End of menu controller definition
