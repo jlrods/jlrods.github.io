da924d4b7c1700bc1b74b9343687f5d66a71cdb5
Jose <Jose@MILLENIUMHAWCK>
2017-03-04 13:09:51 +0000

    Add interactivity for inst media

---
+++ Interactive-Education.js
@@ -0,0 +1,27 @@
+﻿// Variables to control the text change within buttons
+var btn1=false; btn2 = false;
+
+$(document).ready(function () {
+    //jquery to control click function on the Check this media buttons
+    $('#btnUSB').click(function () {
+        if (btn1 === false) {
+            $(this).html("<b>Hide this multi-media!</b>");
+            btn1 = true;
+        } else {
+            $(this).html("<b>Check this extra media!</b>");
+            btn1 = false;
+        }
+        $("#videoDivUSB").slideToggle();
+    });
+
+    $('#btnUNIMET').click(function () {
+        if (btn2 === false) {
+            $(this).html("<b>Hide this multi-media!</b>");
+            btn2 = true;
+        } else {
+            $(this).html("<b>Check this extra media!</b>");
+            btn2 = false;
+        }
+        $("#videoDivUNIMET").slideToggle();
+    });
+});
