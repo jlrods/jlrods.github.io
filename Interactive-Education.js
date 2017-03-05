4d0e1ed25162082f5235752465ed1f5e8719ca23
Jose <Jose@MILLENIUMHAWCK>
2017-03-05 13:48:28 +0000

    Fix footer positioning and proof read

--- Interactive-Education.js
+++ Interactive-Education.js
@@ -5,23 +5,23 @@ $(document).ready(function () {
     //jquery to control click function on the Check this media buttons
     $('#btnUSB').click(function () {
         if (btn1 === false) {
-            $(this).html("<b>Hide this multi-media!</b>");
+            $(this).html("<b>Hide this video!</b>");
             btn1 = true;
         } else {
-            $(this).html("<b>Check this extra media!</b>");
+            $(this).html("<b>Check this video!</b>");
             btn1 = false;
         }
-        $("#videoDivUSB").slideToggle();
+        $("#videoDivUSB").slideToggle('slow');
     });
 
     $('#btnUNIMET').click(function () {
         if (btn2 === false) {
-            $(this).html("<b>Hide this multi-media!</b>");
+            $(this).html("<b>Hide this video!</b>");
             btn2 = true;
         } else {
-            $(this).html("<b>Check this extra media!</b>");
+            $(this).html("<b>Check this video!</b>");
             btn2 = false;
         }
-        $("#videoDivUNIMET").slideToggle();
+        $("#videoDivUNIMET").slideToggle('slow');
     });
 });
