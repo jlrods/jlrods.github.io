3288f65e23263dbf106f8ebb5b1782f57864da05
Jose <Jose@MILLENIUMHAWCK>
2017-03-13 21:11:52 +0000

    Add Logos CSS Interaction for galleries

---
+++ Interactive-Career.js
@@ -0,0 +1,14 @@
+﻿function ChangeEmpImage(callerID) {
+    var img;
+    var frame = "ImageDisplay";
+    if (callerID.toLowerCase().includes("optel")) {
+        img = document.getElementById("OptelImageDisplay");
+        img.src = callerID.concat(".jpg");
+    } else if (callerID.toLowerCase().includes("fh")) {
+        img = document.getElementById("FHImageDisplay");
+        img.src = callerID.concat(".jpg");
+    } else if(callerID.toLowerCase().includes("tdv")){
+        img = document.getElementById("ToyotaImageDisplay");
+        img.src = callerID.concat(".jpg");
+    }
+}
