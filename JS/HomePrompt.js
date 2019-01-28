5444567699d4e05341705216c88bfcedbd28d648
Jose Rodriguez <jose.rodriguez@optelgroup.com>
2019-01-28 13:04:27 +0000

    Introudce BS4 to Home page

---
+++ JS/HomePrompt.js
@@ -0,0 +1,4 @@
+﻿//Function to show a message to run the unknown scripts
+$(document).ready(function () {
+    window.alert("Please, take note this website is under maintenance, so appearance of some webpages might be different");
+});
