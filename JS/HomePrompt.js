e609773f8eaac5d1f096820e72a4309485fe152a
Jose Rodriguez <jose.rodriguez@optelgroup.com>
2019-03-03 14:58:12 +0000

    Include Downloads page + Bigger Home gallery

--- JS/HomePrompt.js
+++
@@ -1,4 +0,0 @@
-﻿//Function to show a message to run the unknown scripts
-$(document).ready(function () {
-    window.alert("Please, take into account that this website is currently under maintenance, so appearance of some webpages might be different. Thanks in advance.");
-});
